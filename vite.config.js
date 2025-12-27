6371318effa766b940f0f365a863dbe9485d5748
Frédérick <frederick@MacBook-Air-de-Frederick.local>
2025-12-27 01:38:31 +0100

    Conformité index + PWA (vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Lid - E-commerce',
+        short_name: 'Lid',
+        description: 'Plateforme e-commerce moderne pour l’achat et la vente de produits en ligne.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
