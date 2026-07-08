324e3519d237cda758efe061e50e616d27d8d3c0
Frederick Ahobaut <sitefinovas@gmail.com>
2026-07-08 12:50:04 +0200

    build: update production dist assets
    update all compiled production assets, replace deprecated hashed asset files, update index.html to use new entrypoint, add new service modules and remove old unused assets

---
+++ dist/lid/assets/useAppConfig-McTHJHBN.js
@@ -0,0 +1,1 @@
+import{u as e}from"./query-vendor-BJ9h4sCv.js";import{c as p}from"./index-Dq3NJIsN.js";async function r(){return(await p.get("/api/v1/public/app-config")).data}function t(){return e({queryKey:["public-app-config"],queryFn:r,staleTime:5*60*1e3,retry:1})}export{t as u};
